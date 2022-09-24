22b45d96843cf420c699183da22add4239330aef
ISLAN SILVA FIGUEREDO <unififigueredo>
2022-09-23 22:07:15 -0300

    corrected issues and branch

--- src/main/java/com/aleislan/ticket/machine/core/Troco.java
+++ src/main/java/com/aleislan/ticket/machine/core/Troco.java
@@ -6,7 +6,7 @@ import java.util.Iterator;
  *
  * @author Calebe de Paula Bianchini
  */
-class Troco {
+final class Troco {
 
     protected PapelMoeda[] papeisMoeda;
     protected int lengthMoeda = 6;
@@ -17,68 +17,26 @@ class Troco {
         int count = 0;
 
         //Como o argumento quantidade de PapelMoeda  é int, será arredondado sempre para baixo a divisão       
-        papeisMoeda[5] = PapelMoeda(100, valor / 100);
+        papeisMoeda[5] = new PapelMoeda(100, valor / 100);
         valor = valor - (papeisMoeda[5].getQuantidade() * 100);
 
-        papeisMoeda[4] = PapelMoeda(50, valor / 50);
+        papeisMoeda[4] = new PapelMoeda(50, valor / 50);
         valor = valor - (papeisMoeda[4].getQuantidade() * 50);
 
-        papeisMoeda[3] = PapelMoeda(20, valor / 20);
+        papeisMoeda[3] = new PapelMoeda(20, valor / 20);
         valor = valor - (papeisMoeda[3].getQuantidade() * 20);
 
-        papeisMoeda[2] = PapelMoeda(10, valor / 10);
+        papeisMoeda[2] = new PapelMoeda(10, valor / 10);
         valor = valor - (papeisMoeda[2].getQuantidade() * 10);
 
-        papeisMoeda[1] = PapelMoeda(5, valor / 5);
+        papeisMoeda[1] = new PapelMoeda(5, valor / 5);
         valor = valor - (papeisMoeda[1].getQuantidade() * 5);
 
-        papeisMoeda[0] = PapelMoeda(2, valor / 2);
-        valor = valor - (papeisMoeda[0].getQuantidade() * 2);
-
+        papeisMoeda[0] = new PapelMoeda(2, valor / 2);
+ 
         arrayTroco = getIterator();
 
     }
-
-        // while (valor % 100 != 0) {
-        //     valor = valor - 100;
-        //     count++;
-        // }
-        // papeisMoeda[5] = new PapelMoeda(100, count);
-
-        // count = 0;
-        // while (valor % 50 != 0) {
-        //     valor = valor - 50;
-        //     count++;
-        // }
-        // papeisMoeda[4] = new PapelMoeda(50, count);
-
-        // count = 0;
-        // while (valor % 20 != 0) {
-        //     valor = valor - 20;
-        //     count++;
-        // }
-        // papeisMoeda[3] = new PapelMoeda(20, count);
-
-        // count = 0;
-        // while (valor % 10 != 0) {
-        //     valor = valor - 10;
-        //     count++;
-        // }
-        // papeisMoeda[2] = new PapelMoeda(10, count);
-
-        // count = 0;
-        // while (valor % 5 != 0) {
-        //     valor = valor - 5;
-        //     count++;
-        // }
-        // papeisMoeda[1] = new PapelMoeda(5, count);
-
-        // count = 0;
-        // while (valor % 2 != 0) {
-        //     valor = valor - 2;
-        //     count++;
-        // }
-        // papeisMoeda[0] = new PapelMoeda(2, count);
 
     public Iterator<PapelMoeda> getIterator() {
         return new TrocoIterator(this);
@@ -113,11 +71,7 @@ class Troco {
 
         @Override
         public boolean hasNext() {
-            if(this.indexArray < troco.lengthMoeda) {
-                return true;
-            } else {
-                return false;
-            }
+            return this.indexArray < troco.lengthMoeda;
         }
 
         @Override
